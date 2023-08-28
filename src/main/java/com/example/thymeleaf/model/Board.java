d1212f1ce7ce8f63bc4638a1a9c82bee42af2c4c
지민재 <wigus02@naver.com>
2023-08-28 18:43:07 +0900

    init

--- src/main/java/com/example/thymeleaf/model/Board.java
+++ src/main/java/com/example/thymeleaf/model/Board.java
@@ -1,10 +1,13 @@
 package com.example.thymeleaf.model;
 
+import com.sun.istack.NotNull;
 import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
 
 @Entity
 @Data
@@ -12,7 +15,9 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(min=2,max=30,message = "제목은 2자이상 30자 이하입니다.")
     private String title;
     private String content;
+    @Size(max=10)
     private String writer;
 }
